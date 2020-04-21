71bf634aae8577231d044fb09c2610ea6f48167f
NL-William <58902590+NL-William@users.noreply.github.com>
2020-04-21 17:23:14 +0200

    Drop IE11 support, add some other good security headers

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const app = express();
 const port = 3000;
 
 app.use((req, res, next) => {
-    res.setHeader("Content-Security-Policy", "script-src 'unsafe-inline' 'nonce-12345'");
-    res.setHeader("X-Content-Security-Policy", "script-src 'unsafe-inline' 'nonce-12345'"); // IE11
+    res.setHeader("Content-Security-Policy", "script-src 'unsafe-inline' 'nonce-12345'");  
+    //res.setHeader("X-Content-Security-Policy", "script-src 'unsafe-inline' 'nonce-12345'"); -- You really don't want to support IE11
+  
+    //Set some other headers to make it production alike
+    res.setHeader("X-Frame-Options", "deny");
+    res.setHeader("X-XSS-Protection", "1; mode=block");
+    res.setHeader("X-Content-Type-Options", "nosniff");
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.setHeader("Referrer-Policy", "strict-origin-when-cross-origin");
+    res.setHeader("Feature-Policy", "vibrate 'none'; geolocation 'none'");
+    
     return next();
   });
 
